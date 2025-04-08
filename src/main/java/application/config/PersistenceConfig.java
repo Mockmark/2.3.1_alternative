15b5710b50c06f46c6fec8879587bb3c5a38ab07
Michael Mark <selfdevourment@gmail.com>
2025-04-08 21:57:26 +0500

    Correct MySQLDialect.

--- src/main/java/application/config/PersistenceConfig.java
+++ src/main/java/application/config/PersistenceConfig.java
@@ -60,7 +60,7 @@ public class PersistenceConfig {
     Properties additionalProperties() {
         Properties properties = new Properties();
         properties.setProperty("hibernate.hbm2ddl.auto", "update");
-        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
         properties.setProperty("hibernate.show_sql", "true");
 
         return properties;
